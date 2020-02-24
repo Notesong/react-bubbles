46d04f293ce18efd4d1c2d80565ab9f663951c70
Lee Damien <notesong.git@outlook.com>
2020-02-23 21:20:21 -0500

    Answers to questions and login form with Auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,65 @@
-import React from "react";
+import React, { useState } from 'react';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, showLoader] = useState(false);
+  const [error, setError] = useState('');
+
+  const formReset = () => {
+    showLoader(false);
+    setUsername('');
+    setPassword('');
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    
+    setError('');
+    showLoader(true);
+
+    axiosWithAuth()
+      .post("/login", { username: username, password: password })
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        formReset();
+        history.push('/protected');
+      })
+      .catch(err => {
+        setError('Incorrect username or password.');
+        localStorage.removeItem("token");
+        formReset();
+      });
+  };
+
   return (
-    <>
+    <div className="login">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <div className="login-container">
+        <form className="form" onSubmit={onSubmit}>
+          {error && <p className="error">{error}</p>}
+          <p>Login</p>
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={e => setUsername(e.currentTarget.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            autoComplete="new-password"
+            value={password}
+            onChange={e => setPassword(e.currentTarget.value)}
+          />
+          <button className="submit" type="submit" disabled={isLoading}>
+            {isLoading ? 'Logging in...' : 'Log In'}
+          </button>
+        </form>
+        <p>Lambda School</p>
+        <p>i&lt;3Lambd4</p>
+      </div>
+    </div>
   );
-};
-
-export default Login;
+}
